9b1f1430fba3e56212fbd02bce5353911f9076aa
Isaac de Souza <isaacde2012@gmail.com>
2019-11-21 21:12:18 -0300

    pequenos ajustes na main para deixar o usuário escolher o nome do arquivo.

--- trabalhoED/main.c
+++ trabalhoED/main.c
@@ -1,8 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "avh.h"
+
 int main(void) {
-  //lendo a lista e criando a árvore 
-  //interagindo com o usuário
- leitura("experimental.txt");
+char* p = (char*) malloc(sizeof(char)*100);
+ printf("digite o nome do arquivo de entrada.");
+ scanf(" %[^\n]",p);
+ leitura(p);
 
 
   return 0;
